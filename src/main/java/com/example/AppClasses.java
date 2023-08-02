b4e875c38013dcd0f50ef70068bbb38c4f8d20c6
David Henrique da Costa <davidcostanewlife@gmail.com>
2023-08-02 01:39:49 +0000

    classes2

--- src/main/java/com/example/AppClasses.java
+++ src/main/java/com/example/AppClasses.java
@@ -1,10 +1,30 @@
 package com.example;
 
 class Cliente {
-    double renda;
-    char sexo;
-    int anoNascimento;
+    private double renda;
+    private char sexo;
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+    private int anoNascimento;
 
+    double getRenda(){
+        return renda;
+    }
+    void setRenda(double renda){
+        if (renda >= 0)
+            this.renda = renda;
+            else System.out.println("O valor da renda deve ser maior que 0");
+    }
 
 }
 
@@ -15,12 +35,22 @@ public class AppClasses {
         System.out.println("criador de classes");
 
         Cliente cliente1 = new Cliente();
-        cliente1.renda = 10000;
-        cliente1.sexo = 'M';
-        cliente1.anoNascimento = 1991;
+        cliente1.setRenda(10000);
+        cliente1.setSexo('M');
+        cliente1.setAnoNascimento(1991);
         
-        System.out.println("renda "+ cliente1.renda );
-        System.out.println("sexo "+ cliente1.sexo );
-        System.out.println("Ano de Nascimento"+ cliente1.anoNascimento );
+        System.out.println("renda "+ cliente1.getRenda());
+        System.out.println("sexo "+ cliente1.getSexo());
+        System.out.println("Ano de Nascimento "+ cliente1.getAnoNascimento());
+
+
+        Cliente cliente2 = new Cliente();
+        cliente2.setRenda(-800);
+        cliente2.setSexo('F');
+        cliente2.setAnoNascimento(1995);
+
+        System.out.println("renda "+ cliente2.getRenda() );
+        System.out.println("sexo "+ cliente2.getSexo());
+        System.out.println("Ano de Nascimento "+ cliente2.getAnoNascimento());
     }
 }
