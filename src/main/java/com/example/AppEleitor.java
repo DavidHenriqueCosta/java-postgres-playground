c8c8c05f07048ac9355d45befbfa09e65b7bb104
David Henrique da Costa <davidcostanewlife@gmail.com>
2023-08-08 11:56:39 +0000

    cliente

---
+++ src/main/java/com/example/AppEleitor.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.time.LocalDate;
+
+public class AppEleitor {
+    public static void main(String[] args) {
+
+        var cidadao = new Cidadao();
+        cidadao.setDataNascimento(LocalDate.of(1949,9,30));
+        System.out.println(cidadao.idade());
+        System.out.println(cidadao.eleitor());
+    }
+}
